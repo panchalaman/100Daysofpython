7629ff86244fd70e5d75c3dd774903aff455adff
Aman <aman.panchal@gisma-student.com>
2025-02-07 23:42:41 +0100

    Done

--- Day 10/Multiple Return Values/task.py
+++ Day 10/Multiple Return Values/task.py
@@ -1,4 +1,5 @@
 def format_name(f_name, l_name):
+    """This function formats the given string of first and last name to title case."""
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
