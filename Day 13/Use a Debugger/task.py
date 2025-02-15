3e9c3914a7e346c4bd452087aa5358f55a3ea158
Aman <aman.panchal@gisma-student.com>
2025-02-15 22:17:42 +0100

    Done

--- Day 13/Use a Debugger/task.py
+++ Day 13/Use a Debugger/task.py
@@ -1,15 +1,14 @@
 import random
 import maths
 
-
 def mutate(a_list):
     b_list = []
-    new_item = 0
+    new = []
     for item in a_list:
         new_item = item * 2
         new_item += random.randint(1, 3)
-        new_item = maths.add(new_item, item)
-    b_list.append(new_item)
+        new.append(maths.add(new_item, item))
+    b_list.append(new)
     print(b_list)
 
 
